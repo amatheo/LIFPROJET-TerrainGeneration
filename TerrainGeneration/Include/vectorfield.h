34df1b3bbe1ae212d98d59aab5ae2f1b59b23529
amatheo <59826260+amatheo@users.noreply.github.com>
2021-10-07 11:03:10 +0200

    fix : added inline vector accessor

--- TerrainGeneration/Include/vectorfield.h
+++ TerrainGeneration/Include/vectorfield.h
@@ -11,6 +11,20 @@ public:
 	vector<Vector> v;
 	VectorField(vector<Vector> vectorArray);
 	VectorField(int size);
+
+	//Access Vector
+	Vector operator[] (int) const;
+	Vector& operator[] (int);
 };
+
+inline Vector VectorField::operator[](int i) const
+{
+	return v[i];
+}
+
+inline Vector& VectorField::operator[](int i)
+{
+	return v[i];
+}
 
 #endif
