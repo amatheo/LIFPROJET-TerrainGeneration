f7287680c406dc000b44032ba6ef42d96bf2e556
Corentin Favier <cofavier@sfr.fr>
2021-10-15 16:55:25 +0200

    :refactor: added some comment to scalar-field.cpp/h
    Clarity changes

--- TerrainGeneration/Source/scalar-field.cpp
+++ TerrainGeneration/Source/scalar-field.cpp
@@ -5,8 +5,29 @@ ScalarField::ScalarField() {
 	this->nx = 0;
 	this->ny = 0;
 }
+ScalarField::ScalarField(int width, int length, int nbX, int nbY) {
+	//dummy example of how the box si filled
+	this->nx = nbX;
+	this->ny = nbY;
+	int segmentx = width / (this->nx - 1);
+	int segmenty = width / (this->ny - 1);
+
+	int position;
+	for (int y = 0; y < this->ny; y++) {
+
+		for (int x = 0; x < this->nx; x++) {
+			position = getIndex(x, y);
+			this->box[position] = Vector(segmentx*x, segmenty*y, 0);
+			this->heightVector[position] = 0;
+		}
+	}
+
+
+}
+
 
 float ScalarField::getHeight(int x, int y) {
+	//donne la hauteur du point en x y, stocké dans heightVector
 	int index = getIndex(x, y);
 	return this->heightVector[index];
 }
@@ -22,5 +43,6 @@ float ScalarField::slope(int x, int y) {
 }
 
 int ScalarField::getIndex(int x, int y) {
+	//since box is one dimensionnal, we need to calculate where the data for a point in x y is stored
 	return (x * ny) + y;
 }
