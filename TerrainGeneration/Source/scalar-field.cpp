1f6945f4256261d801a1b12dc629cf7a8e51a232
Corentin Favier <cofavier@sfr.fr>
2021-10-16 16:14:07 +0200

    Update scalar-field.cpp

--- TerrainGeneration/Source/scalar-field.cpp
+++ TerrainGeneration/Source/scalar-field.cpp
@@ -17,7 +17,7 @@ ScalarField::ScalarField(int width, int length, int nbX, int nbY) {
 
 		for (int x = 0; x < this->nx; x++) {
 			position = getIndex(x, y);
-			this->box[position] = Vector(segmentx*x, segmenty*y, 0);
+			this->box[position] = Vector(segmentx * x, segmenty * y, 0);
 			this->heightVector[position] = 0;
 		}
 	}
@@ -27,14 +27,56 @@ ScalarField::ScalarField(int width, int length, int nbX, int nbY) {
 
 
 float ScalarField::getHeight(int x, int y) {
-	//donne la hauteur du point en x y, stocké dans heightVector
+	//gives height of the point in x y, stored in heightVector
 	int index = getIndex(x, y);
 	return this->heightVector[index];
 }
 
 Vector ScalarField::gradient(int x, int y) {
-	double gradient = ((y + 1) - y) / ((x + 1) - x);
-	return Vector(gradient);
+	//the 4 points surrounding the original point
+	int prevXId;
+	int prevYId;
+	int succXId;
+	int succYId;
+	if (x == 0) {
+		prevXId = getIndex(x, y);
+	}
+	else {
+		prevXId = getIndex(x - 1, y);
+	}
+	if (x == nx) {
+		succXId = getIndex(x, y);
+	}
+	else {
+		succXId = getIndex(x + 1, y);
+	}
+	if (y == 0) {
+		prevYId = getIndex(x, y);
+	}
+	else {
+		prevYId = getIndex(x, y - 1);
+	}
+	if (y == ny) {
+		succYId = getIndex(x, y);
+	}
+	else {
+		succYId = getIndex(x, y + 1);
+	}
+	int gradX = (heightVector[succXId] - heightVector[prevXId]) / (2 * box[ny][0]);
+		
+	int gradY = (heightVector[succYId] - heightVector[prevYId]) / (2 * box[1][1]);
+	double fullGrad = gradX / gradY;
+	return Vector(fullGrad);
+
+
+
+
+	//think when calculating at borders(0, or nx/ny)
+
+
+	//legacy text, dont want to lose it if change is bad
+	//double gradient = ((y + 1) - y) / ((x + 1) - x);
+	//return Vector(gradient);
 }
 
 float ScalarField::slope(int x, int y) {
